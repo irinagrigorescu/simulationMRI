3d9c2e91cb2b9db93ba0befa7778524dd3cfb56d
Irina Grigorescu <irina@eduroam-int-dhcp-97-21-6.ucl.ac.uk>
2017-07-06 14:51:25 +0100

    changed to have phase angle

--- src/gui/ch3/ch3flip.m
+++ src/gui/ch3/ch3flip.m
@@ -22,7 +22,7 @@ function varargout = ch3flip(varargin)
 
 % Edit the above text to modify the response to help ch3flip
 
-% Last Modified by GUIDE v2.5 28-Nov-2016 20:42:27
+% Last Modified by GUIDE v2.5 06-Jul-2017 14:11:33
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -77,7 +77,7 @@ function varargout = ch3flip_OutputFcn(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
-varargout{1} = handles.output;
+varargout{1} = handles.output
 
 % % % % % % HANDLES
 % handles = 
@@ -92,6 +92,8 @@ varargout{1} = handles.output;
 %             uipanel1: [1x1 Panel]
 %                axes1: [1x1 Axes]
 %           speedOfSim: [1x1 UIControl]
+%               text17: [1x1 UIControl]
+%      RFphaseangleval: [1x1 UIControl]
 %           rotateB1mx: [1x1 UIControl]
 %           rotateB1my: [1x1 UIControl]
 %            rotateB1x: [1x1 UIControl]
@@ -179,7 +181,8 @@ end
 
 % % % % Call appropriate function
 
-phaseAngle = 0.0 * pi / 180; % RF phase angle
+% RF phase angle
+phaseAngle = str2double(get(handles.RFphaseangleval, 'String')) * pi / 180;
 dw = 0; % off-resonance frequency in Hz
 
 
@@ -509,6 +512,29 @@ function speedOfSim_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function speedOfSim_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to speedOfSim (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function RFphaseangleval_Callback(hObject, eventdata, handles)
+% hObject    handle to RFphaseangleval (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of RFphaseangleval as text
+%        str2double(get(hObject,'String')) returns contents of RFphaseangleval as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function RFphaseangleval_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to RFphaseangleval (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
