4fbeab19fbf31b510f696af5a89a189fe1ca2add
Irina Grigorescu <irina@igrigorescu2.cs.ucl.ac.uk>
2017-08-11 17:15:51 +0100

    added some separation between different scenarios

--- src/learning/test_simEchoes.m
+++ src/learning/test_simEchoes.m
@@ -33,21 +33,30 @@ videoFlag.nameVideo = 'echoSim-CPMG-video';
 % % % % % % % % % % % RUN SIMULATION:
 simEchoes(N, T1, T2, dws, rfa, pha, tau, Ttotal, videoFlag);
                             
-% % % % % % % 2. Spin Echo Classic
-% % % % EVENTS
-% rfa = [90, 180]; % flip angles
-% pha = [ 0,  0 ]; % phase angles
-% tau = [ 0,  2]; % time before them
-% Npulses = length(rfa);       % how many
-% Ttotal  = 4 * sum(tau); % total time of simulation
-% % % % SPINS:
-% N   =  1000;
-% dw  =  2.1*pi/(2*pi*min(tau(tau~=0))); % full circle dephasing over 
-%                                        % smallest tau
-% dws = linspace(-dw, dw, N); % create a population of spins with equally
-%                             % distributed off-resonances
+%% % % % % % % % 2. Spin Echo Classic
+% % RELAXATION TERMS:
+T1 =   200;
+T2 =    30;
+% % % EVENTS
+rfa = [90, 150]; % flip angles
+pha = [ 0,  0 ]; % phase angles
+tau = [ 0,  2 ]; % time before them
+Npulses = length(rfa);       % how many
+Ttotal  = 4 * sum(tau); % total time of simulation
+% % % SPINS:
+N   =  1000;
+dw  =  2.1*pi/(2*pi*min(tau(tau~=0))); % full circle dephasing over 
+                                       % smallest tau
+dws = linspace(-dw, dw, N); % create a population of spins with equally
+                            % distributed off-resonances
+% % VIDEO:
+videoFlag.flag      = 0;    % % 0 = Don't make video
+videoFlag.nameVideo = 'echoSim-SEclassic-video';
 
-% % % % % % % 3. Spin Echo 8 ball - the 2nd 90 produces a spin echo with
+% % % % % % % % % % % RUN SIMULATION:
+simEchoes(N, T1, T2, dws, rfa, pha, tau, Ttotal, videoFlag);
+
+%% % % % % % % % 3. Spin Echo 8 ball - the 2nd 90 produces a spin echo with
 % % reduced amplitude
 % % % % EVENTS
 % rfa = [90,  90]; % flip angles
@@ -62,7 +71,8 @@ simEchoes(N, T1, T2, dws, rfa, pha, tau, Ttotal, videoFlag);
 % dws = linspace(-dw, dw, N); % create a population of spins with equally
 %                             % distributed off-resonances
 
-% % % % % % % 4. Stimulated Echo
+
+%% % % % % % % % 4. Stimulated Echo
 % % Magnetization is ?stored? along z-direction 
 % % between second and third pulse (so-called ?phase memory?)
 % % % % RELAXATION TERMS
@@ -81,8 +91,8 @@ simEchoes(N, T1, T2, dws, rfa, pha, tau, Ttotal, videoFlag);
 % dws = linspace(-dw, dw, N); % create a population of spins with equally
 %                             % distributed off-resonances
 
-%%
-% % % % % % % 5. 3 RF PULSES AND 5 ECHOES
+
+%% % % % % % % % 5. 3 RF PULSES AND 5 ECHOES
 % % % % RELAXATION TERMS
 % T1 =   200;
 % T2 =    80;
